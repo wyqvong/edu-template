0d601b128cbce13957fba2bdef0aa1d6392800ad
wyqvong <874744558@qq.com>
2020-02-16 18:00:07 +0800

    配置使用nunjucks模板引擎布局项目

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import express from 'express'
 import config from './config'
+import nunjucks from 'nunjucks'
 
 const app = express()
 
 app.use('/node_modules', express.static(config.node_modules_path))
 app.use('/public', express.static(config.public_path))
 
-app.set('views', config.viewPath)
-app.set('view engine',  'ejs')
-
-app.get('/', (req, res) => {
-    res.render('index')
+//配置使用nunjucks模板引擎
+nunjucks.configure(config.viewPath, {
+    autoescape: true,
+    express: app
 })
 
 
-app.listen(3000,()=>{
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+
+app.listen(3000, () => {
     console.log('server is running at port 3000')
 })
