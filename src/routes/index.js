94ffc25e6d09f504753022855a64b3cbc716545d
wyqvong <874744558@qq.com>
2020-02-22 14:13:35 +0800

    提取处理函数到controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 import express from 'express'
-
+import * as indexController from '../controllers/index'
 
 const router = express.Router()
 
 
-router.get('/', (req, res, next) => {
-    res.render('index.html')
-})
-
+router
+    .get('/', indexController.showIndex)
+    
 
 export default router
